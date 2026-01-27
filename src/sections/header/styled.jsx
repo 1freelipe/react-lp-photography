2b4b33c9c1599939bbbe52964a200eecd4fa5013
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-01-27 16:32:49 -0300

    ajustes na responsividade da imagem da header em aplicativos menores

--- src/sections/header/styled.jsx
+++ src/sections/header/styled.jsx
@@ -279,7 +279,7 @@ export const DivImage = styled(motion.div)`
     z-index: -100;
   }
 
-  @media (min-width: 370px) and (max-width: 400px) {
+  @media (min-width: 360px) and (max-width: 400px) {
     img {
       position: absolute;
       top: 13%;
